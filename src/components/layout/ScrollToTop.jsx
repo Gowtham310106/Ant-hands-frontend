6c694f828b7b1b37ae749f2dc71c851c9483ba5e
Gowtham <gowtham310106@gmail.com>
2025-12-09 00:23:51 +0530

    added scroll to top

---
+++ src/components/layout/ScrollToTop.jsx
@@ -0,0 +1,20 @@
+// src/components/layout/ScrollToTop.jsx
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    // Scroll to top on every route change
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth' // Smooth scroll animation
+    })
+  }, [pathname])
+
+  return null
+}
+
+export default ScrollToTop
